f24ea2928aa48cd78113c84518a3d479b485c3e3
AnagramMC <hisham.ata@hotmail.com>
2020-02-06 12:32:48 -0500

    cube added

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello World");
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(0.0f, 1000, 0.0f));
     }
 
     // Update is called once per frame
